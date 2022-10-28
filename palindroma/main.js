3b758124de0889c527a33c3200e0bfb200f989b7
NicholasPeara92 <nicholas.peara@gmail.com>
2022-10-28 16:07:26 +0200

    Aggiunge funzione

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,2 +1,19 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+const word = prompt("Inserisci una parola");
+
+function palindrome(str) {
+    
+    for (let i = 0; i < str.length; i++) {
+        
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+        
+    }
+    return true;  
+}
+
+console.log(palindrome(word));
